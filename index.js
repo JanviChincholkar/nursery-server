2774ef61142fde5ae3af434c27e7f386ac67e037
Janvi Chincholkar <janvichincholkar14@gmail.com>
2024-07-25 21:09:42 +0530

    port

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 app.use(express.json())
@@ -203,7 +205,7 @@ app.delete("/plant/:id" ,(req, res) =>{
     })
 })
 
-const PORT = 5000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
